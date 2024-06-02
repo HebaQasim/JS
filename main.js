7755e5784daf0ee4fc9e7b3592311c99eab3d5c5
HebaQasim <hebaqasem3002@gmail.com>
2024-06-02 21:37:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+(document.querySelector(".userProfile h2").textContent="Hello Heba");
+console.log(document.querySelector(".userInfo p"));
+document.querySelector(".userInfo p").classList.add("dark");
+console.log(document.querySelector(".userProfile h2"));
